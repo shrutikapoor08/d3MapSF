5a1b5e18788cb8bc43f68f3ed785a0e0dd16c474
Shruti Kapoor <shruti.kapoor@telus.com>
2017-06-25 23:31:25 -0700

    live updating location of N route

--- script.js
+++ script.js
@@ -2,32 +2,40 @@
   var svg;
   var projection;
   renderSFMap()
-    .then(svg => renderRoutes(svg));
+    .then(function(){
+      renderVehicles();
+      updateLocations();
+      renderRoutes();
+    });
 }());
 
 function renderSFMap() {
   return new Promise((resolve, reject) => {
-    const width = 900;
-    const height = 750;
-    const offset = [width / 1.9, height / 1.3];
+    const width = 720;
+    const height = 700;
+    const offset = [width / 2, height / 2];
+
+    //code snippet referenced from 3rd party
     projection = d3.geoMercator()
       .scale(160000)
       .translate(offset)
       .center([-122.43, 37.75]);
+    //end 3rd party code snippet
 
     svg = d3.select('body')
       .append('svg')
       .attr('height', height)
-      .attr('width', width);
+      .attr('width', width)
+      .attr('viewBox', "60 70 500 450");
 
     const path = d3.geoPath().projection(projection);
     d3.selectAll('path').attr('d', path);
 
-    d3.json('sfmaps/neighborhoods.json', (json) => {
+    d3.json('sfmaps/neighborhoods.json', (neighborhoods) => {
       svg.append('g')
         .attr('id', 'neighborhoods')
         .selectAll('path')
-        .data(json.features)
+        .data(neighborhoods.features)
         .enter()
         .append('path')
         .attr('id', d => d.properties.neighborho)
@@ -37,7 +45,35 @@ function renderSFMap() {
   });
 }
 
-function renderRoutes(svg) {
+function renderVehicles(){
+  const xhr = new XMLHttpRequest();
+  const url = 'http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&r=N&t=1144953500233';
+  xhr.open('GET', url);
+  xhr.send();
+  xhr.onload = function() {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        let vehicles = xhr.response;
+        vehicles = JSON.parse(vehicles).vehicle;
+
+        const g = svg.insert('g', '#neighborhoods + *')
+          .attr('id', 'vehicles');
+        const circles = g.selectAll('circle')
+          .data(vehicles)
+          .enter()
+          .append('circle');
+
+        const circleAttributes = circles
+          .attr('cx', data => projection([data.lon, data.lat])[0])
+          .attr('cy', data => projection([data.lon, data.lat])[1])
+          .attr('r', 4)
+          .style('fill', 'yellow');
+      }
+    }
+  }
+};
+
+function renderRoutes() {
   const xhr = new XMLHttpRequest();
   const url = 'http://webservices.nextbus.com/service/publicJSONFeed?command=routeConfig&a=sf-muni&r=N';
   xhr.open('GET', url);
@@ -56,11 +92,15 @@ function renderRoutes(svg) {
           .append('circle');
 
         const circleAttributes = circles
-          .attr('cx', d => projection([d.lon, d.lat])[0])
-          .attr('cy', d => projection([d.lon, d.lat])[1])
-          .attr('r', 3)
-          .style('fill', 'red');
+          .attr('cx', data => projection([data.lon, data.lat])[0])
+          .attr('cy', data => projection([data.lon, data.lat])[1])
+          .attr('r', 2)
+          .style('fill', 'black');
       }
     }
   };
+}
+
+function updateLocations(){
+   setInterval(function() {console.log('renderroutes'); renderVehicles(); }, 15000);
 }
